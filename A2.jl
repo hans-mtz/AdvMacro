a410a00d5a7543a8f1419717431859e35346af99
Hans <53349767+hans-mtz@users.noreply.github.com>
2020-10-04 15:18:58 -0400

    Update Assignment 2. Added MPB method and times table.

--- A2.jl
+++ A2.jl
@@ -1,8 +1,8 @@
 # setting working directory
 # cd("/Volumes/SSD Hans/Github/MacroFall2020/AdvMacro")
 # mkpath("Assignment2")
-cd("/Volumes/SSD Hans/Github/MacroFall2020/AdvMacro/Assignment2")
- mkpath("graphs")
+# cd("/Volumes/SSD Hans/Github/MacroFall2020/AdvMacro/Assignment2")
+# mkpath("graphs")
 
 
 # Pkg.add("Parameters")
@@ -13,6 +13,9 @@ using Parameters
 #Pkg.add("IntervalArithmetic")
 #Pkg.add("IntervalRootFinding")
 using Roots
+using IntervalArithmetic, IntervalRootFinding
+
+
 
 # defining parameters
 
@@ -30,7 +33,7 @@ using Roots
         χ::Float64
         l::Float64
         M = ((z*α*β)/(1-β+β*δ))^(1/(1-α))
-        N = z*M^(α)-δ*M
+        N = (z*M^α)-δ*M
         # VFI Paramters
         max_iter::Int64   = 2000  ; # Maximum number of iterations
         dist_tol::Float64 = 1E-9  ; # Tolerance for distance
@@ -38,9 +41,7 @@ using Roots
         H_tol::Float64    = 1E-9  ; # Tolerance for policy function iteration
     end
 
-    # Allocate paramters to object p for future calling
 
-    p = Par(l= 0.4, χ =1.0)
 
 # Getting the right χ
 function get_me_chi(l,p::Par)
@@ -49,21 +50,21 @@ function get_me_chi(l,p::Par)
     chi = find_zero(f, 1E-9)
     return chi
 end
-
+    # Allocate paramters to object p for future calling
 chi = get_me_chi(0.4,p)
 
 p = Par(l= 0.4, χ=40.2)
 
-# Getting the right χ
 
-using IntervalArithmetic, IntervalRootFinding
 
 function get_me_l(k,kp,p::Par)
     @unpack z, α, β, M, N, σ, η, χ = p
     f(l) = z*(1-α)*(k^α)/((z*(k^α)*(l^(1-α))-kp)^σ)-χ*(l^(η+α))
-    A = range(0,1;length=100)
+    A = range(0,1;length=200)
     B = f.(A)
-    l = minimum(B[B.>=0])
+    index = findmin(B[B.>=0])[2]
+    l = A[index]
+    return l
 end
 
 get_me_l(k_grid_20[1],k_grid_20[G_kp_20][1],p)
@@ -105,7 +106,7 @@ function path(k,kp,l,p::Par)
     r = (a,b) -> α*(a^(a-1))*(b^(1-α))
     y = (a,b) -> (a^α)*b^(1-α)
     c = (a,b,d) -> y(a,b)-d
-    l = get_me_l.(k,k_p,fill(p,length(k)))
+    l = get_l.(k,k_p,fill(p,length(k)))
     wage = w.(k,l)
     int = r.(k,l)
     cons = c.(k,kp,l)
@@ -117,9 +118,9 @@ end
 # Utility function
 function utility(k,kp,l,p::Par)
     @unpack z, α, χ, η, σ = p
-    y = (a,b) -> (a^α)*b^(1-α)
+    y = (a,b) -> z*(a^α)*b^(1-α)
     c = (a,b,d) -> y(a,b)-d
-    utils = (c(k,l,kp).^(1-σ))/(1-σ)-(χ*l.^(η+1)/(η+1))
+    utils = (c(k,l,kp).^(1-σ)/(1-σ))-(χ*l.^(η+1)/(η+1))
     return utils
 end
 
@@ -185,18 +186,18 @@ end
         G_kp = fill(0,n_k)
         G_l = fill(0.0,n_k)
         for i = 1:n_k
-            V_aux = zeros(n_k) ; # Empty vector for auxiliary value of V(i,j)
-            G_l_aux = zeros(n_k)
+            V_aux = zeros(n_k) ;
+            l_aux = zeros(n_k) # Empty vector for auxiliary value of V(i,j)
             for j = 1:n_k
                 # Evaluate potential value function for combinations of
                 # current capital k_i and future capital k_j
-                G_l_aux[j] = get_me_l(k_grid[i],k_grid[j],p)
-                V_aux[j] = utility(k_grid[i],k_grid[j],G_l_aux[j],p) + β*V_old[j]
+                l_aux[j] = get_l(k_grid[i],k_grid[j],p)
+                V_aux[j] = utility(k_grid[i],k_grid[j],l_aux[j],p) + β*V_old[j]
                 #println(V_aux[j]," ",k_grid[i]," ",k_grid[j]," ",utility(k_grid[i],k_grid[j],z,a,b) + b*V_old[j])
             end
             # Choose maximum value given current capital k_i
             V[i], G_kp[i] = findmax(V_aux)
-            G_l[i] = G_l_aux[G_kp[i]]
+            G_l[i] = l_aux[G_kp[i]]
         end
         return V, G_kp, G_l
     end
@@ -206,6 +207,8 @@ end
 
 # Solve VFI with grid search and loops
     function Solve_VFI_loop(n_k,p::Par)
+        println("----------------------------------------------------")
+        println("Value Function Iteration - Grid Search with Loops")
         # Get Grid
         k_grid = Make_K_Grid(n_k,p)
         # Solve VFI
@@ -214,44 +217,15 @@ end
         return V,G_kp, G_l, k_grid
     end
 
-    # Execute Numerical VFI
-    @time V_20, G_kp_20, G_l_20, k_grid_20 = Solve_VFI_loop(20,p)
-    @time V_50, G_kp_50, G_l_50, k_grid_50 = Solve_VFI_loop(50,p)
-    @time V_200, G_kp_200, G_l_200,k_grid_200 = Solve_VFI_loop(200,p)
-    #@time V_1000, G_kp_1000, G_l_1000, k_grid_1000 = Solve_VFI_loop(1000,p)
-
-    gr()
-    # Value Function Analytical vs 200
-        plot(k_grid_20,V_20,linetype=:scatter,marker=(:diamond,3), msw=0 , label="VFI - n_k=50")
-        plot!(k_grid_50,V_50,linetype=:scatter,marker=(:diamond,3),msw=0 , label = "VFI - n_k=20")
-        plot!(k_grid_200,V_200,linetype=:scatter,marker=(:diamond,3),msw=0 , label = "VFI - n_k=200")
-        #plot!(k_grid_1000,V_1000,linetype=:scatter,marker=(:diamond,3),msw=0 , label = "VFI - n_k=1000")
-        xlabel!("Capital")
-        ylabel!("Value")
-        title!("Value Fn VFI")
-        savefig("graphs/VFI_V")
-
-
-    # Capital Policy Function Analytical vs 200
-        plot(k_grid_200,k_grid_200,lw=1,linecolor=RGB(0.6,0.6,0.6),label=nothing)
-        plot!(k_grid_50,k_grid_50[G_kp_50],linetype=:scatter,marker=(:diamond,3),msw=0 ,label="VFI - n_k=50")
-        plot!(k_grid_20,k_grid_20[G_kp_20],linetype=:scatter,marker=(:diamond,3),msw=0 ,label = "VFI - n_k=20")
-        plot!(k_grid_200,k_grid_200[G_kp_200],linetype=:scatter,marker=(:diamond,3),msw=0 ,label = "VFI - n_k=200")
-        #plot!(k_grid_1000,k_grid_1000[G_kp_1000],linetype=:scatter,marker=(:diamond,3),msw=0 ,label = "VFI - n_k=1000")
-        xlabel!("Capital")
-        ylabel!("Capital")
-        title!("Capital VFI")
-        savefig("graphs/VFI_cap")
-
 
     # Euler Equation
     function Euler_Error(k,kp,kpp,p::Par)
         # Return percentage error in Euler equation
         @unpack z, α, β, σ, l = p
         lp = l
-        LHS = ((z*kp^α*lp^(1-α)-kpp)/(z*k^α*l^(1-α)-kp))^σ
-        RHS = β*α*z*kp^(α-1)*lp^(1-α)
-        return (RHS/LHS-1)*100
+        LHS = 1/((z*k^α*l^(1-α))-kp)^σ
+        RHS = β*α*z*kp^(α-1)*lp^(1-α)/(z*kp^α*lp^(1-α)-kpp)
+        return ((RHS/LHS)-1)*100
     end
 
     # Euler error by number of points in grid
@@ -269,21 +243,7 @@ end
         return Euler
     end
 
-    Euler_20 = Euler_grid(k_grid_20,G_kp_20)
-    Euler_50 = Euler_grid(k_grid_50,G_kp_50)
-    Euler_200 = Euler_grid(k_grid_200,G_kp_200)
-    #Euler_1000 = Euler_grid(k_grid_1000,G_kp_1000)
 
-    # Graphing Euler Error 200
-    plot([0,2*k_ss],[0,0],lw=1,linecolor=RGB(0.6,0.6,0.6),label=nothing,title = "Euler Equation Error (%)")
-    plot!(k_grid_50,Euler_50,linetype=:scatter,marker=(:diamond,3),msw=0 ,label="VFI - n_k=50")
-    plot!(k_grid_20,Euler_20,linetype=:scatter,marker=(:diamond,3),msw=0 ,label="VFI - n_k=20")
-    plot!(k_grid_200,Euler_200,linetype=:scatter,marker=(:diamond,3),msw=0 ,label="VFI - n_k=200")
-    #plot!(k_grid_20,Euler_1000,linetype=:scatter,marker=(:diamond,3),msw=0 ,label="VFI - n_k=1000")
-    xlabel!("Capital")
-    ylabel!("Percentage Points")
-    title!("Euler Error VFI")
-    savefig("graphs/VFI_Euler")
 
 #-----------------------------------------------------------
     # VFI - Grid Search - Matrices - Howard's Policy Iteration
@@ -308,7 +268,7 @@ end
             end
             # Recover Policy Functions
             G_kp   = [G_kp[i][2] for i in 1:n_k] # G_kp is a Cartesian index
-            G_l = get_me_l.(k_grid,k_grid[G_kp],fill(p,n_k))
+            G_l = get_l.(k_grid,k_grid[G_kp],fill(p,n_k))
 
             # Return output
             return V, G_kp, G_l
@@ -317,56 +277,85 @@ end
 
     # Solve VFI with Howard's policy iteration
         function Solve_VFI_HPI(n_H,n_k,p::Par)
+            println("----------------------------------------------------")
+            println("Value Function Iteration - Howard's Policy Iteration")
             # Get Grid
             k_grid = Make_K_Grid(n_k,p)
             # Utility matrix
-            U_mat = [utility(k_grid[i],k_grid[j],get_me_l(k_grid[i],k_grid[j],p),p) for i in 1:n_k, j in 1:n_k]
+            U_mat = [utility(k_grid[i],k_grid[j],get_l(k_grid[i],k_grid[j],p),p) for i in 1:n_k, j in 1:n_k]
             # Solve VFI
             V, G_kp, G_l = VFI_grid(x->HT_grid_mat(x,U_mat,k_grid,p,n_H),k_grid,p)
             # Return Solution
             return V,G_kp, G_l, k_grid
         end
 
-        # Execute Numerical VFI
-        @time V_20, G_kp_20, G_l_20, k_grid_20 = Solve_VFI_HPI(20,20,p)
-        @time V_50, G_kp_50, G_l_50, k_grid_50 = Solve_VFI_HPI(20,50,p)
-        @time V_200, G_kp_200, G_l_200, k_grid_200 = Solve_VFI_HPI(20,200,p)
-        #@time V_1000, G_kp_1000, k_grid_1000 = Solve_VFI_HPI(20,1000,p)
-
-        # Value Function Plot
-            plot(k_grid_20,V_20,linetype=:scatter,marker=(:diamond,3), msw=0 , label="VFI - n_k=50")
-            plot!(k_grid_50,V_50,linetype=:scatter,marker=(:diamond,3),msw=0 , label = "VFI - n_k=20")
-            plot!(k_grid_200,V_200,linetype=:scatter,marker=(:diamond,3),msw=0 , label = "VFI - n_k=200")
-            #plot!(k_grid_1000,V_1000,linetype=:scatter,marker=(:diamond,3),msw=0 , label = "VFI - n_k=1000")
-            xlabel!("Capital")
-            ylabel!("Value")
-            title!("Value Function VFI-HPI")
-            savefig("graphs/HPI_V")
 
 
-        # Capital Policy Function
-            plot(k_grid_200,k_grid_200,lw=1,linecolor=RGB(0.6,0.6,0.6),label=nothing)
-            plot!(k_grid_50,k_grid_50[G_kp_50],linetype=:scatter,marker=(:diamond,3),msw=0 ,label="VFI - n_k=50")
-            plot!(k_grid_20,k_grid_20[G_kp_20],linetype=:scatter,marker=(:diamond,3),msw=0 ,label = "VFI - n_k=20")
-            plot!(k_grid_200,k_grid_200[G_kp_200],linetype=:scatter,marker=(:diamond,3),msw=0 ,label = "VFI - n_k=200")
-            #plot!(k_grid_1000,k_grid_1000[G_kp_1000],linetype=:scatter,marker=(:diamond,3),msw=0 ,label = "VFI - n_k=1000")
-            xlabel!("Capital")
-            ylabel!("Capital")
-            title!("Capital VFI-HPI")
-            savefig("graphs/HPI_Cap")
 
-            Euler_20 = Euler_grid(k_grid_20,G_kp_20)
-            Euler_50 = Euler_grid(k_grid_50,G_kp_50)
-            Euler_200 = Euler_grid(k_grid_200,G_kp_200)
-            #Euler_1000 = Euler_grid(k_grid_1000,G_kp_1000)
+#-----------------------------------------------------------
 
-            # Graphing Euler Error 200
-            plot([0,2*k_ss],[0,0],lw=1,linecolor=RGB(0.6,0.6,0.6),label=nothing,title = "Euler Equation Error (%)")
-            plot!(k_grid_50,Euler_50,linetype=:scatter,marker=(:diamond,3),msw=0 ,label="VFI - n_k=50")
-            plot!(k_grid_20,Euler_20,linetype=:scatter,marker=(:diamond,3),msw=0 ,label="VFI - n_k=20")
-            plot!(k_grid_200,Euler_200,linetype=:scatter,marker=(:diamond,3),msw=0 ,label="VFI - n_k=200")
-            #plot!(k_grid_20,Euler_1000,linetype=:scatter,marker=(:diamond,3),msw=0 ,label="VFI - n_k=1000")
-            xlabel!("Capital")
-            ylabel!("Percentage Points")
-            title!("Euler Error VFI-HPI")
-            savefig("graphs/HPI_Euler")
+
+# Fixed point with MPB
+    function VFI_grid_MPB(T::Function,k_grid,p::Par)
+        @unpack β, max_iter, dist_tol = p
+        # Initialize variables for loop
+        n_k    = length(k_grid) ; # Number of grid nodes
+        V_old  = zeros(n_k)     ; # Initial value, a vector of zeros
+        iter   = 0              ; # Iteration index
+        V_dist = 1              ; # Initialize distance
+        println(" ")
+        println("------------------------")
+        println("VFI - Grid Search - MPB - n_k=$n_k")
+        for iter=1:max_iter
+            # Update value function
+            V_new, G_kp, G_l = T(V_old)
+            # MPB and Distance
+            MPB_l  = β/(1-β)*minimum(V_new-V_old)
+            MPB_h  = β/(1-β)*maximum(V_new-V_old)
+            V_dist = MPB_h - MPB_l
+            # Update old function
+            V_old  = V_new
+            # Report progress
+            if mod(iter,100)==0
+                println("   VFI Loop: iter=$iter, dist=",100*V_dist,"%")
+            end
+            # Check Convergence
+            if (V_dist<=dist_tol)
+                # Recover value and policy functions
+                V = V_old .+ (MPB_l+MPB_h)/2
+                # Return
+                println("VFI - Grid Search - MPB - n_k=$n_k")
+                println("Iterations = $iter and Distance = ",100*V_dist)
+                println("------------------------")
+                println(" ")
+                return V, G_kp, G_l
+            end
+        end
+        # Report error for non-convergence
+        error("Error in VFI - Grid Search - MPB - Solution not found")
+    end
+
+    # Define function for Value update and policy functions
+        function T_grid_mat(V_old,U_mat,k_grid,p::Par)
+            @unpack z, α, β = p
+            n_k    = length(V_old)
+            V,G_kp = findmax( U_mat .+ β*repeat(V_old',n_k,1) , dims=2 )
+            G_kp   = [G_kp[i][2] for i in 1:n_k] # G_kp is a Cartesian index
+            G_l = get_l.(k_grid,k_grid[G_kp],fill(p,n_k))
+            return V, G_kp, G_l
+        end
+
+
+# Solve VFI with MPB
+    function Solve_VFI_MPB(n_k,p::Par)
+        println("----------------------------------------------------")
+        println("Value Function Iteration - MacQueen-Porteus Bounds")
+        # Get Grid
+        k_grid = Make_K_Grid(n_k,p)
+        # Utility matrix
+        U_mat = [utility(k_grid[i],k_grid[j],get_l(k_grid[i],k_grid[j],p),p) for i in 1:n_k, j in 1:n_k]
+        # Solve VFI
+        V, G_kp, G_l = VFI_grid_MPB(x->T_grid_mat(x,U_mat,k_grid,p),k_grid,p)
+        # Return Solution
+        return V,G_kp, G_l, k_grid
+    end
